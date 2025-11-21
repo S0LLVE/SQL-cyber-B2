67cf5bdbd9b58261c2247100f17c778349221381
sebastien sve <sebastien.soave@student.csb.school>
2025-11-21 12:16:46 +0100

    Exercice-11

---  update-employee.sql
+++  update-employee.sql
@@ -1,6 +1,4 @@
-Employee Robert King just got a promotion !
-You must update his function in the table employees.
-
-His new job Title : IT Manager
-
-His new superior : Adams Andrew
+UPDATE employees
+SET Title = 'IT Manager',
+    ReportsTo = (SELECT EmployeeId FROM employees WHERE FirstName = 'Adams' AND LastName = 'Andrew')
+WHERE FirstName = 'Robert' AND LastName = 'King';
