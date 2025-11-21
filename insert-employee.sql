6316d71def2ec21f09ced2759c71acb133791246
sebastien sve <sebastien.soave@student.csb.school>
2025-11-21 12:12:13 +0100

    Exercice-9

--- insert-employee.sql
+++ insert-employee.sql
@@ -1,32 +1,31 @@
-   INSERT INTO employees (
-       LastName, 
-       FirstName, 
-       Title, 
-       ReportsTo, 
-       BirthDate, 
-       HireDate, 
-       Address, 
-       City, 
-       State, 
-       Country, 
-       PostalCode, 
-       Phone, 
-       Fax, 
-       Email
-   ) VALUES (
-       'Doe', 
-       'John', 
-       'IT Developer', 
-       (SELECT EmployeeId FROM employees WHERE FirstName = 'Michael' AND LastName = 'Mitchell'), 
-       '03-18-1999', 
-       '03-18-2020', 
-       '142 rue Sainte-Catherine', 
-       'Bordeaux', 
-       'NA', 
-       'France', 
-       '33000', 
-       '+33 6 77 88 99 10', 
-       '+33 5 68 89 90 90', 
-       'john.doe@gmail.com'
-   );
-   
+INSERT INTO employees (
+    LastName, 
+    FirstName, 
+    Title, 
+    ReportsTo, 
+    BirthDate, 
+    HireDate, 
+    Address, 
+    City, 
+    State, 
+    Country, 
+    PostalCode, 
+    Phone, 
+    Fax, 
+    Email
+) VALUES (
+    'Doe', 
+    'John', 
+    'IT Developer', 
+    (SELECT EmployeeId FROM employees WHERE FirstName = 'Michael' AND LastName = 'Mitchell'), 
+    '1999-03-18 00:00:00',  -- Format de date corrigé
+    '2020-03-18 00:00:00',  -- Format de date corrigé
+    '142 rue Sainte-Catherine', 
+    'Bordeaux', 
+    'NA', 
+    'France', 
+    '33000', 
+    '+33 6 77 88 99 10', 
+    '+33 5 68 89 90 90', 
+    'john.doe@gmail.com'
+);
