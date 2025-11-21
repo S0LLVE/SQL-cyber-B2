307af2ea3aa5f9dfadbcbb5b72f9a264a4aee98a
sebastien sve <sebastien.soave@student.csb.school>
2025-11-21 17:45:17 +0100

    JOINS-SQL

--- select-customers-where-and.sql
+++ select-customers-where-and.sql
@@ -1,4 +1,5 @@
-SELECT FirstName, LastName
-FROM customers
-WHERE Country = 'USA' 
-AND SupportRep = 'Peacock Jane';
+SELECT c.FirstName, c.LastName
+FROM customers c
+JOIN support_reps sr ON c.SupportRepId = sr.SupportRepId
+WHERE c.Country = 'USA' 
+AND sr.Name = 'Peacock Jane';
