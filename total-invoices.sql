55cacc3e1e1f9b2a50acd40af5fccc83495fc45d
sebastien sve <sebastien.soave@student.csb.school>
2025-11-23 23:34:28 +0100

    FUNCTIONS-SQL

---
+++ total-invoices.sql
@@ -0,0 +1,14 @@
+SELECT
+  UPPER(C.FirstName || ' ' || C.LastName) AS FullName,
+  SUM(I.Total) AS AllInvoices
+FROM customers AS C
+JOIN invoices AS I
+  ON C.CustomerId = I.CustomerId
+GROUP BY
+  C.CustomerId,
+  C.FirstName,
+  C.LastName
+HAVING
+  SUM(I.Total) > 38
+ORDER BY
+  FullName ASC;
