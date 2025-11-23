d372d6bed41544bfd36991225102f35b72fd2caf
sebastien sve <sebastien.soave@student.csb.school>
2025-11-23 02:47:51 +0100

    ADVANCED-SQL

--- customers-continent.sql
+++ customers-continent.sql
@@ -4,9 +4,10 @@ SELECT
   Country,
   CASE
     WHEN Country IN ('USA', 'Canada', 'Mexico', 'Brazil', 'Argentina', 'Colombia', 'Chile') THEN 'America'
-    WHEN Country IN ('France', 'Germany', 'UK', 'Spain', 'Italy', 'Netherlands', 'Belgium', 'Sweden', 'Norway', 'Finland', 'Denmark', 'Switzerland', 'Austria', 'Ireland', 'Portugal', 'Greece', 'Poland') THEN 'Europe'
+    -- Ajout des pays manquants à la liste de l'Europe
+    WHEN Country IN ('France', 'Germany', 'UK', 'Spain', 'Italy', 'Netherlands', 'Belgium', 'Sweden', 'Norway', 'Finland', 'Denmark', 'Switzerland', 'Austria', 'Ireland', 'Portugal', 'Greece', 'Poland', 'Czech Republic', 'Hungary', 'United Kingdom') THEN 'Europe'
     WHEN Country IN ('Japan', 'China', 'India', 'South Korea', 'Indonesia', 'Thailand', 'Vietnam', 'Philippines', 'Malaysia', 'Singapore') THEN 'Asia'
     WHEN Country IN ('Australia', 'New Zealand') THEN 'Oceania'
-    ELSE 'Other' -- Pour les pays non listés, vous pouvez choisir une autre catégorie ou null
+    ELSE 'Other'
   END AS Continent
 FROM customers;
