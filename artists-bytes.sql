9878490bbaaecdc4f6ae052b9c3b9948502c5a76
sebastien sve <sebastien.soave@student.csb.school>
2025-11-23 03:19:48 +0100

    ADVANCED-SQL

--- artists-bytes.sql
+++ artists-bytes.sql
@@ -2,7 +2,7 @@ SELECT
   AR.Name AS ArtistName,
   AL.Title AS AlbumName,
   T.Name AS TrackName,
-  printf('%.2f MB', ROUND(T.Bytes / 1000000.0, 2)) AS MegaBytes
+  REPLACE(REPLACE(printf('%.2f MB', ROUND(T.Bytes / 1000000.0, 2)), '.00 MB', ' MB'), '0 MB', ' MB') AS MegaBytes -- Tentative pour supprimer '.00'
 FROM tracks AS T
 JOIN albums AS AL
   ON T.AlbumId = AL.AlbumId
