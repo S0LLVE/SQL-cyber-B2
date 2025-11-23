82cb0a8cf126a6f2551073df8f275c08b9203026
sebastien sve <sebastien.soave@student.csb.school>
2025-11-23 03:24:16 +0100

    ADVANCED-SQL

--- artist-albums-case.sql
+++ artist-albums-case.sql
@@ -8,7 +8,6 @@ SELECT
     THEN 'Productive'
     WHEN COUNT(AL.AlbumId) >= 10
     THEN 'Very Productive'
-    -- Note : la clause ELSE 'Unproductive' pour NbAlbums = 0 n'est plus nécessaire car HAVING les exclut
   END AS IsProductive
 FROM artists AS A
 LEFT JOIN albums AS AL
@@ -19,5 +18,5 @@ GROUP BY
 HAVING
   COUNT(AL.AlbumId) > 0 -- Exclure les artistes qui n'ont aucun album
 ORDER BY
-  A.Name ASC -- Gardons A.Name ASC comme hypothèse la plus probable après A.ArtistId
+  A.ArtistId ASC -- Ordre par ID de l'artiste
 LIMIT 100;
