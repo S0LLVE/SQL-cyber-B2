86580bdf87b17a9f20825346d3df6b3f30376020
sebastien sve <sebastien.soave@student.csb.school>
2025-11-23 03:22:16 +0100

    ADVANCED-SQL

--- artist-albums-case.sql
+++ artist-albums-case.sql
@@ -8,7 +8,7 @@ SELECT
     THEN 'Productive'
     WHEN COUNT(AL.AlbumId) >= 10
     THEN 'Very Productive'
-    ELSE 'Unknown' -- Au cas où un artiste n'aurait pas d'albums (bien que COUNT donnerait 0)
+    ELSE 'Unproductive' -- Traite les artistes avec 0 album comme "Unproductive"
   END AS IsProductive
 FROM artists AS A
 LEFT JOIN albums AS AL
@@ -17,5 +17,5 @@ GROUP BY
   A.ArtistId,
   A.Name
 ORDER BY
-  A.ArtistId
+  A.Name ASC -- Tri par nom d'artiste
 LIMIT 100;
