a5f3b7225eb611cf8e5536d5155273494e67027c
sebastien sve <sebastien.soave@student.csb.school>
2025-11-23 03:17:50 +0100

    ADVANCED-SQL

--- artists-bytes.sql
+++ artists-bytes.sql
@@ -2,7 +2,7 @@ SELECT
   AR.Name AS ArtistName,
   AL.Title AS AlbumName,
   T.Name AS TrackName,
-  printf('%.2f MB', ROUND(T.Bytes / 1048576.0, 2)) AS MegaBytes
+  printf('%.2f MB', ROUND(T.Bytes / 1000000.0, 2)) AS MegaBytes
 FROM tracks AS T
 JOIN albums AS AL
   ON T.AlbumId = AL.AlbumId
